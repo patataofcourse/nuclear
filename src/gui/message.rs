28e3b369939514a792517cd9527c6c05c2e0cf3a
patataofcourse <patataofcourse@gmail.com>
2022-11-04 12:37:25 +0100

    replace native_dialog with tinyfiledialogs

--- src/gui/message.rs
+++ src/gui/message.rs
@@ -1,52 +1,22 @@
-use native_dialog::{MessageDialog, MessageType};
+//use native_dialog::{MessageDialog, MessageType};
+use tinyfiledialogs::{MessageBoxIcon, YesNo};
 
 pub fn info(title: &str, contents: &str) {
-    match MessageDialog::new()
-        .set_type(MessageType::Info)
-        .set_title(title)
-        .set_text(contents)
-        .show_alert()
-    {
-        Ok(_) => {}
-        Err(_) => println!("INFO: {}\n{}\n\n", title, contents),
-    };
+    tinyfiledialogs::message_box_ok(title, contents, MessageBoxIcon::Info);
 }
 
-pub fn confirm(title: &str, contents: &str) -> bool {
-    match MessageDialog::new()
-        .set_type(MessageType::Info)
-        .set_title(title)
-        .set_text(contents)
-        .show_confirm()
-    {
-        Ok(c) => c,
-        Err(_) => panic!("Cannot open a confirmation prompt"),
-    }
+pub fn yes_no(title: &str, contents: &str) -> bool {
+    tinyfiledialogs::message_box_yes_no(title, contents, MessageBoxIcon::Question, YesNo::No)
+        == YesNo::Yes
 }
 
 pub fn warning(title: &str, contents: &str) {
-    match MessageDialog::new()
-        .set_type(MessageType::Warning)
-        .set_title(title)
-        .set_text(contents)
-        .show_alert()
-    {
-        Ok(_) => {}
-        Err(_) => println!("WARNING: {}\n{}\n\n", title, contents),
-    };
+    tinyfiledialogs::message_box_ok(title, contents, MessageBoxIcon::Warning);
 }
 pub fn error(title: &str, contents: &str) {
-    match MessageDialog::new()
-        .set_type(MessageType::Error)
-        .set_title(title)
-        .set_text(contents)
-        .show_alert()
-    {
-        Ok(_) => {}
-        Err(_) => println!("ERROR: {}\n{}\n\n", title, contents),
-    };
+    tinyfiledialogs::message_box_ok(title, contents, MessageBoxIcon::Error);
 }
 
-pub fn file_picker() {
-    todo!();
+pub fn file_picker() -> Option<String> {
+    tinyfiledialogs::open_file_dialog("a", "/", None)
 }
